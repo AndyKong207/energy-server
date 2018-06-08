48aed3d6baed1e1eccbc0634866613da43e0b1b6
AndyKong207 <andy_kong@163.com>
2018-06-08 15:08:44 +0800

    a

--- app/middleware/getRequestPath.js
+++ app/middleware/getRequestPath.js
@@ -1,6 +1,7 @@
 module.exports = () => {
   return async function (ctx, next) {
     console.log('request path::::', ctx.request.path)
+    ctx.logger.info('request path::::: %j', ctx.request.path);
     await next();
     // 上报请求时间
   }
